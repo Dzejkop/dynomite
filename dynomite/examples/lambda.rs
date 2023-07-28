b1b04f2812ebc94ad53a353d75ac2545e14a5d98
Dzejkop <jakubtrad@gmail.com>
2023-07-28 11:30:39 +0200

    Update deps

--- dynomite/examples/lambda.rs
+++ dynomite/examples/lambda.rs
@@ -1,9 +1,8 @@
-use dynomite::{
-    dynamodb::{DynamoDb, DynamoDbClient},
-    retry::Policy,
-    Retries,
-};
-use lambda_http::{handler, lambda_runtime};
+use dynomite::dynamodb::{DynamoDb, DynamoDbClient};
+use dynomite::retry::Policy;
+use dynomite::Retries;
+use lambda_http::lambda_runtime::LambdaEvent;
+use lambda_http::{lambda_runtime, service_fn};
 
 type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
 
@@ -11,14 +10,16 @@ type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
 async fn main() -> Result<(), Error> {
     let client = DynamoDbClient::new(Default::default()).with_retries(Policy::default());
 
-    lambda_runtime::run(handler(move |_, _| {
+    lambda_runtime::run(service_fn(move |_event: LambdaEvent<()>| {
         let client = client.clone();
+
         async move {
             let tables = client
                 .list_tables(Default::default())
                 .await?
                 .table_names
                 .unwrap_or_default();
+
             Ok::<_, Error>(tables.join("\n"))
         }
     }))
